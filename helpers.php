650b641c021cd8fd4bfe7eb3323b3c3686110f43
arafat-zip <powergenyt@gmail.com>
2025-06-11 07:48:54 +0600

    first initilaized to home view

---
+++ helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * get the base path
+ * @param string path
+ * @return string
+ */
+
+ function basePath($path = '') {
+    return __DIR__ . '/' . $path;
+
+ }
+
+//  var_dump(basePath());
+//  die();
